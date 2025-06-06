11e874709ec349ecbe3a7b22c712c7b8c169fd0e
kenshima <kenneth.r.shima@gmail.com>
2025-06-06 17:11:46 +0200

    took out cors for localhost

--- source/php/index.php
+++ source/php/index.php
@@ -7,7 +7,7 @@ require "bootstrap.php";
 
 use Src\Controller\BookingController;
 
-header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Origin: http:localhost/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
